6fd25d8d70cb1c3a7567fc6ecb42b6e6834d5436
kathiravan <85435269+kathirvijay@users.noreply.github.com>
2025-04-06 20:29:28 +0530

    Update msal.service.spec.ts

--- client/src/app/msal.service.spec.ts
+++ client/src/app/msal.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MsalService } from './msal.service';
+import { MsalService } from '@azure/msal-angular';
 
 describe('MsalService', () => {
   let service: MsalService;
