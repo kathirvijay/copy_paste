dd12d4b546c728c72d84b801026222bb000adb03
kathiravan <rpkathir17@gmail.com>
2025-04-06 20:49:09 +0530

    add 06-04-2k25 - (002)

--- client/src/app/msal.service.spec.ts
+++ client/src/app/msal.service.spec.ts
@@ -1,8 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-<<<<<<< HEAD
-=======
-
->>>>>>> 6fd25d8d70cb1c3a7567fc6ecb42b6e6834d5436
 import { MsalService } from '@azure/msal-angular';
 
 describe('MsalService', () => {
